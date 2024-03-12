af63dd8a90272a4d1fd0491ad17d7302044472b0
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-03-12 23:49:59 +0700

    doctest finish

--- circle.py
+++ circle.py
@@ -9,6 +9,12 @@ class Circle:
         
         :param radius: radius of the circle, may be zero.
         :raises ValueError: if radius is negative.
+
+        test negative radius
+        >>> Circle(-1)
+        Traceback (most recent call last):
+        ...
+        ValueError: radius must be non-negative
         """
         if radius < 0:
             raise ValueError("radius must be non-negative")
@@ -19,6 +25,10 @@ class Circle:
         area of this circle and another circle.
         Since area is pi*r**2, the radii of the 3 circles
         should form a Pythagorean triple (r1^2 + r2^2 = r3^2)
+
+         Examples:
+        >>> Circle(3).add_area(Circle(4))
+        Circle(5.0)
         """
         r1 = self.get_radius()
         r2 = circle.get_radius()
